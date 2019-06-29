9b0ba1570f6fddb5f17aec652d224a7f76131145
danilomo <danilomendoncaoliveira@gmail.com>
2019-06-29 19:25:17 +0200

    Initial draft of category implementation without metaclasses

--- financespy/categories.py
+++ financespy/categories.py
@@ -1,100 +1,68 @@
-import sys
 
-_categories = {}
-_instances = {}
-
-def get_category( str_ ):
-    str_ = str_.lower()
-
-    if str_ not in _categories or "uncategorized" == str_:
-        return _uncategorized
-
-    category = _categories[str_]
-
-    if str_ not in _instances:
-        _instances[str_] = category()
-
-    return _instances[str_]
-
-
-
-class CategoryMeta(type):
-    def __init__(cls, name, parents, dct):
-        _categories[name.lower()] = cls
-
-        if hasattr( cls, "subtypes" ):
-            for subtype in cls.subtypes:
-                globals()[subtype] = type(subtype, (cls,), {'subtypes' : []})
-
-        super(CategoryMeta, cls).__init__(name, parents, dct)
-
-class Category(metaclass = CategoryMeta):
+class Category:
+    
+    def __init__(self, name, parent = None):
+        self._name = name
+        self._parent = parent
+        
     def __str__(self):
-        return self.__class__.__name__.lower()
+        return self._name
 
-    def is_of_category(self,cat):
-        return isinstance(cat, self.__class__)
+    def matches(self,cat):
+        if self._name == cat:
+            return True
 
+        if self._parent is None:
+            return False
+
+        return self._parent.is_of_category(cat)
+    
     __repr__ = __str__
 
+class Categories:
+
+    def __init__(self,categories, default_category):
+        self._categories = categories
+        self._default = default_category
+
+    def category(self, category):
+        return self._categories.get(
+            category,
+            self._default
+        )
+
+    
+def categories_from_list(cats):
+    
+    def aux(catmap, cats, parent):
+        for cat in cats:
+            if type(cat) is str:
+                category = Category(
+                    cat,
+                    parent
+                )
+                catmap[cat] = category
+            elif type(cat) is tuple:
+                category = Category(
+                    cat[0],
+                    parent
+                )
+                catmap[cat[0]] = category
+                aux(catmap, cat[1], category)
+
+    catmap = {}
+    aux(catmap, cats, None)
+    return Categories(catmap, catmap["uncategorized"])
+                
+    
+default_categories = [
+    "misc",
+    "uncategorized",
+    ("food", [ "groceries", "restaurant", "street_food"])
+]
 
 
-class Expense(Category):
-    pass
+c = categories_from_list(default_categories)
 
-class Uncategorized(Expense):
-    pass
-
-class Income(Category):
-    pass
-
-_uncategorized = Uncategorized()
-
-
-class Misc(Expense):
-    pass
-
-class Unexpected(Expense):
-    pass
-
-class Fixed(Expense):
-    pass
-
-class Food(Expense):
-    pass
-
-class Utilities(Expense):
-    subtypes = [ "Internet", "Electricity", "Cellphone_Balance" ]
-
-class Travel(Expense):
-    subtypes = [ "Plane_Ticket", "Hotel_Reservation", "Train_Ticket" ]
-
-class Leisure(Expense):
-    pass
-
-class Tax(Expense):
-    subtypes = [ "TV" ]
-
-class Shopping(Expense):
-    subtypes = [ "Electronics", "Clothes", "Sports", "Home_Goods", "Furnishing", "Shopping_Misc", "Shoes", "Bag" ]
-
-class Transport(Expense):
-    subtypes = [ "Monthly_Ticket", "Single_Ticket", "Day_Ticket" ]
-
-class Home(Expense):
-    subtypes = [ "Rent", "Home_Repair", "Home_Service" ]
-
-class Education(Expense):
-    subtypes = [ "Course", "School_Book", "School_Suplies" ]
-
-class German_Course(Course):
-    pass
-
-class Eating_Out(Food):
-    subtypes = [ "Restaurant", "Fast_Food", "Icecream", "Drink", "Street_Food" ]
-
-class Groceries(Food):
-    subtypes = [ "Edeka", "Nahkauf", "Aldi" ]
-
-class Body_And_Hygiene(Expense):
-    subtypes = [ "Perfume", "Hair_Product", "Hairdresser", "Nails" ]
+print(c.category("restaurant").is_of_category("food"))
+print(c.category("restaurant").is_of_category("misc"))
